cb6319b9d2afa5801a0f0bb235261961caa7008c
Joe Thornton <joe.clifford.thornton@gmail.com>
2022-01-24 21:19:42 +0000

    Initial Commit. In order traversal of binary tree

---
+++ PythonTreeTraversal.py
@@ -0,0 +1,38 @@
+class BinaryTreeNode:
+    left = None
+    right = None
+    value = None
+
+    def __init__(self, left, right, value):
+        self.left = left
+        self.right = right
+        self.value = value
+
+def in_order(node, values):
+    if node == None: 
+        return
+    in_order(node.left, values) #Left
+    values.append(node.value) #Visit
+    in_order(node.right, values) #Right
+
+def main():
+    root = BinaryTreeNode(None, None, 4)
+    root.left = BinaryTreeNode(None, None, 2)
+    root.left.left = BinaryTreeNode(None, None, 1)
+    root.left.right = BinaryTreeNode(None, None, 10)
+    root.left.left.left = BinaryTreeNode(None, None, 9)
+    root.left.left.right = BinaryTreeNode(None, None, 13)
+    root.right = BinaryTreeNode(None, None, 6)
+    root.right.left = BinaryTreeNode(None, None, 8)
+    root.right.right = BinaryTreeNode(None, None, 3)
+    root.right.right.left = BinaryTreeNode(None, None, 5)
+    root.right.right.right = BinaryTreeNode(None, None, 7)
+    root.right.right.right.left = BinaryTreeNode(None, None, 12)
+    root.right.right.right.right = BinaryTreeNode(None, None, 11)
+    values = []
+    in_order(root, values)
+    print(values)
+
+
+if __name__ == "__main__":
+    main()
